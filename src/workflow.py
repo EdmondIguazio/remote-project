aa77599496e1bed6d8015977e99fcbf6f77eb2fc
Edmond <69960318+EdmondIguazio@users.noreply.github.com>
2025-08-18 16:08:32 +0300

    Update workflow.py

--- src/workflow.py
+++ src/workflow.py
@@ -1,1 +1,11 @@
+import kfp
+import mlrun
+from kfp import dsl
+
+@kfp.dsl.pipeline()
+def pipeline():
+    
+    project = mlrun.get_current_project()
+    
+    project.run_function("func", handler="handler")
 
