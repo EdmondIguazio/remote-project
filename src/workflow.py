0f8a500538743c1207db72086df94e7171fefe7f
Edmond <69960318+EdmondIguazio@users.noreply.github.com>
2026-03-05 16:02:22 +0200

    Update workflow.py

--- src/workflow.py
+++ src/workflow.py
@@ -11,4 +11,7 @@ def pipeline():
 
     func2 = project.run_function("func2", handler="handler").after(func)
 
-    func_spark = project.run_function("sparktest", handler="handler").after(func2)
+    build_spark = project.build_function(function="sparktest", requirements=["mlrun==1.10.2"], with_mlrun=False).after(func2)
+
+    func_spark = project.run_function("sparktest", handler="handler").after(build_spark)
+
