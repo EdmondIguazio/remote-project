5a15c869b277060c1511284bcec212b802b61672
Edmond <69960318+EdmondIguazio@users.noreply.github.com>
2026-03-05 14:33:32 +0200

    Update workflow.py

--- src/workflow.py
+++ src/workflow.py
@@ -9,6 +9,6 @@ def pipeline():
     
     func = project.run_function("func", handler="handler")
 
-    project.run_function("func2", handler="handler").after(func)
+    func2 = project.run_function("func2", handler="handler").after(func)
 
-
+    func_spark = project.run_function("sparktest", handler="handler").after(func2)
