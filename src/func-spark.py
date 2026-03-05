9224084aa78c083566a8b0248761859cdd6ee85f
Edmond <69960318+EdmondIguazio@users.noreply.github.com>
2026-03-05 14:32:17 +0200

    Update func-spark.py

--- src/func-spark.py
+++ src/func-spark.py
@@ -6,15 +6,4 @@ def handler(context):
   # build spark session
   spark = SparkSession.builder.appName("Spark job").getOrCreate()
 
-  # read csv
-  df = spark.read.load('iris.csv', format="csv",
-                       sep=",", header="true")
-  
-  # sample for logging
-  df_to_log = df.describe().toPandas()
-  
-  # log final report
-  context.log_dataset("df_sample",
-                       df=df_to_log,
-                       format="csv")
   spark.stop()
