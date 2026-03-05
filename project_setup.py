865abfc109c44c62f0884521705a1c9c2888a683
Edmond <69960318+EdmondIguazio@users.noreply.github.com>
2026-03-05 14:30:27 +0200

    Update project_setup.py

--- project_setup.py
+++ project_setup.py
@@ -13,6 +13,18 @@ def setup(project: mlrun.projects.MlrunProject) -> mlrun.projects.MlrunProject:
                         name="func2",
                         kind="job",
                         image="mlrun/mlrun")
+
+    sj = project.set_function(kind="spark", 
+                              func="./src/func-spark.py", 
+                              name="sparktest")
+
+    sj.with_driver_limits(cpu="1300m")
+    sj.with_driver_requests(cpu=1, mem="512m")
+
+    sj.with_executor_limits(cpu="1400m")
+    sj.with_executor_requests(cpu=1, mem="512m")
+
+    sj.with_igz_spark()
     
     project.set_workflow(
         name="workflow", workflow_path="./src/workflow.py", image="mlrun/mlrun-kfp"
